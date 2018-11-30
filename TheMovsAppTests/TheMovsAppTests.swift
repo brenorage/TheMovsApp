6b3cdb564306889878ed634db919780be9eec865
Breno Rage <breno.rage.aboud@accenture.com>
2018-11-30 10:59:39 -0200

    Creating xcode project

---
+++ TheMovsAppTests/TheMovsAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  TheMovsAppTests.swift
+//  TheMovsAppTests
+//
+//  Created by Breno Rage Aboud on 30/11/2018.
+//  Copyright © 2018 Breno Rage Aboud. All rights reserved.
+//
+
+import XCTest
+@testable import TheMovsApp
+
+class TheMovsAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
