1ddf0e77570b09ecbc0152f70041068cfdb8060b
Breno Rage Aboud <brenorage16@gmail.com>
2018-11-30 10:13:59 -0200

    Revert "Feature/viewcode setup"

--- TheMovsApp/Pods/Target Support Files/Pods-TheMovsApp/Pods-TheMovsApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TheMovsApp : NSObject
-@end
-@implementation PodsDummy_Pods_TheMovsApp
-@end
--- TheMovsApp/Pods/Target Support Files/Pods-TheMovsAppTests/Pods-TheMovsAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TheMovsAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_TheMovsAppTests
-@end
