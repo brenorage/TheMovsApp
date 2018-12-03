4f0c142ff93625e5c6411bbf47188dc64a2c1dcd
Breno Rage <breno.rage.aboud@accenture.com>
2018-12-03 17:30:03 -0200

    Cria estrutura de arquivos para a tela de lista de filmes

---
+++ TheMovsApp/Modules/MoviesList/Presenter/MoviesListPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  MoviesListPresenter.swift
+//  TheMovsApp
+//
+//  Created by Breno Rage Aboud on 03/12/2018.
+//  Copyright © 2018 Breno Rage Aboud. All rights reserved.
+//
+
+import Foundation
---
+++ TheMovsApp/Modules/MoviesList/View/MoviesListViewController.swift
@@ -0,0 +1,11 @@
+//
+//  MoviesListViewController.swift
+//  TheMovsApp
+//
+//  Created by Breno Rage Aboud on 03/12/2018.
+//  Copyright © 2018 Breno Rage Aboud. All rights reserved.
+//
+
+import UIKit
+
+class MoviesListViewController: UIViewController {}
