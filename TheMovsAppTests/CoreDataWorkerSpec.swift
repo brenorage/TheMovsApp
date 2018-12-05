058583fca92ade4dea190e4e7d27e6bad8c38a86
André Souza <andre.luiz.de.souza@accenture.com>
2018-12-05 15:53:43 -0200

    Rename CoreDataWorkerSpecs

--- TheMovsAppTests/CoreDataWorkerSpec.swift
+++ TheMovsAppTests/CoreDataWorkerSpec.swift
@@ -10,7 +10,7 @@ import XCTest
 import Nimble
 @testable import TheMovsApp
 
-class CoreDataWorkerSpecs: XCTestCase {
+class CoreDataWorkerSpec: XCTestCase {
     
     var sut: CoreDataWorker!
     
