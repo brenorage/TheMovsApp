fb2bd48325c32c65bf36c11f8324e6c37a86a418
André Souza <andre.luiz.de.souza@accenture.com>
2018-12-05 16:13:39 -0200

    Refactor CoreDataWorker

--- TheMovsApp/Common/CoreData/CoreDataWorker.swift
+++ TheMovsApp/Common/CoreData/CoreDataWorker.swift
@@ -9,14 +9,15 @@
 import CoreData
 
 protocol CoreDataWorkerProtocol {
-    func fetchAll<T: NSManagedObject>(of: T.Type, completion: @escaping (ResultType<[T]>) -> Void)
+    typealias CompletionCallback<T> = (ResultType<T>) -> Void
+    func fetchAll<T: NSManagedObject>(completion: @escaping CompletionCallback<[T]>)
     func save() throws
     func delete(enity: NSManagedObject)
 }
 
 class CoreDataWorker: CoreDataWorkerProtocol {
     
-    func fetchAll<T: NSManagedObject>(of: T.Type, completion: @escaping (ResultType<[T]>) -> Void) {
+    func fetchAll<T>(completion: @escaping (ResultType<[T]>) -> Void) where T : NSManagedObject {
         let fetchRequest = NSFetchRequest<T>(entityName: T.identifier)
         CoreDataManager.shared.persistentContainer.performBackgroundTask ({ (context) in
             do {
@@ -28,7 +29,6 @@ class CoreDataWorker: CoreDataWorkerProtocol {
             }
         })
     }
-    
     
     func save() throws {
         try CoreDataManager.shared.persistentContainer.viewContext.save()
